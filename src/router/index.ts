72e975360e8a93417014ce7c6557d812385c8bdc
kennie-larkson <materialkenny@gmail.com>
2024-02-19 20:34:28 +0100

    created user and post models

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,51 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
+import { UserModel } from "./../models/UserModel";
+import { PostModel, PostType } from "./../models/PostModel";
 
 export const router = Router();
+
+router.get("/users", async (req: Request, res: Response) => {
+  try {
+    res.json({
+      data: await UserModel.all(),
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+router.get("/posts", async (req: Request, res: Response) => {
+  try {
+    res.json({
+      data: await PostModel.all(),
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
+
+router.get("/posts/:id", async (req: Request, res: Response) => {
+  if (!req.params.id) {
+    res.status(400).json({
+      message: "Bad Request",
+    });
+  }
+
+  try {
+    res.json({
+      data: await PostModel.findById<PostType>(req.params.id),
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+});
 
 router.get("/health", (req: Request, res: Response) => {
   res.status(200).send("ok");
