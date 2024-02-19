5983f63e2b47afd6b67a2ebb63d5338e03e6db82
kennie-larkson <materialkenny@gmail.com>
2024-02-19 11:56:09 +0100

    ...

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -15,24 +15,24 @@ import {
 import sequelize from "database.connection";
 import { UUID } from "sequelize";
 
-export class User extends Model<
-  InferAttributes<User>,
-  InferCreationAttributes<User>
-> {
-  // @Attribute(DataTypes.INTEGER)
-  // @PrimaryKey
-  // @AutoIncrement
-  // declare id: CreationOptional<number>;
-  // @Attribute(DataTypes.STRING)
-  // @NotNull
-  // declare firstName: string;
-  // @Attribute(DataTypes.STRING)
-  // declare lastName: string | null;
-  // getFullName() {
-  //   return [this.firstName, this.lastName].join(" ");
-  // }
-}
-//export class User extends Model{}
+// export class User extends Model<
+//   InferAttributes<User>,
+//   InferCreationAttributes<User>
+// > {
+//   // @Attribute(DataTypes.INTEGER)
+//   // @PrimaryKey
+//   // @AutoIncrement
+//   // declare id: CreationOptional<number>;
+//   // @Attribute(DataTypes.STRING)
+//   // @NotNull
+//   // declare firstName: string;
+//   // @Attribute(DataTypes.STRING)
+//   // declare lastName: string | null;
+//   // getFullName() {
+//   //   return [this.firstName, this.lastName].join(" ");
+//   // }
+// }
+export class User extends Model {}
 
 User.init(
   {
