9989d8b1fae3c1707807cf0b93760c7f7487d264
kennie-larkson <materialkenny@gmail.com>
2024-02-19 19:49:06 +0100

    created knex migrations and seeds

---
+++ src/database/migrations/20240219175813_create_users_table.ts
@@ -0,0 +1,17 @@
+import type { Knex } from "knex";
+
+const tableName = "users";
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable(tableName, (table) => {
+    table.increments("id").primary();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("email").notNullable();
+    table.string("password").notNullable();
+    table.timestamps(true, true);
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable(tableName);
+}
